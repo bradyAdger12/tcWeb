c936b9dd5ed774e26e88a4236de4841ad285f3d8
bradyAdger12 <brady@traildock.com>
2022-06-02 17:27:45 -0600

    Change basurl

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ export default {
   ],
 
   axios: {
-    baseURL: 'https://tcweb.onrender.com', // Used as fallback if no runtime config is provided
+    baseURL: 'https://tcapi.onrender.com', // Used as fallback if no runtime config is provided
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
